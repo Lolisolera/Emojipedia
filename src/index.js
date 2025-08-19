94614764b15cd4d75bb5d74461d793918bd8aca3
Lola Marquez <lolisolera101@gmail.com>
2025-08-19 13:28:37 +0100

    Fix React entrypoint: removed raw script tag and updated index.js to use createRoot

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./components/App";
-import './styles.css'; 
+import "./styles.css";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
+root.render(<App />);
 
-ReactDOM.render(<App />, document.getElementById("root"));
